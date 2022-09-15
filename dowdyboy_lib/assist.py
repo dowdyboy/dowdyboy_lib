0e88b17f07692d54651f893bc4177292ba033cb2
dowdyboy <1203550038@qq.com>
2022-09-15 13:51:02 +0800

    添加计算函数运行时间的函数

--- dowdyboy_lib/assist.py
+++ dowdyboy_lib/assist.py
@@ -1,6 +1,7 @@
 import math
 
 
+# 获取卷积输出尺寸
 def get_conv_out_size(in_size, k, s, p):
     h, w = (in_size[0], in_size[1]) if isinstance(in_size, tuple) else (in_size, in_size)
     o_h = math.floor((h + 2 * p - k) / s) + 1
@@ -11,6 +12,7 @@ def get_conv_out_size(in_size, k, s, p):
         return o_h
 
 
+# 获取反卷积输出尺寸
 def get_trans_conv_out_size(in_size, k, s, p):
     h, w = (in_size[0], in_size[1]) if isinstance(in_size, tuple) else (in_size, in_size)
     o_h = s * (h - 1) - 2 * p + k
@@ -21,6 +23,7 @@ def get_trans_conv_out_size(in_size, k, s, p):
         return o_h
 
 
+# 获取空洞卷积输出尺寸
 def get_dilated_conv_out_size(in_size, k, s, p, d):
     h, w = (in_size[0], in_size[1]) if isinstance(in_size, tuple) else (in_size, in_size)
     o_h = math.floor((h + 2 * p - k - (k - 1) * (d - 1)) / s) + 1
@@ -31,5 +34,13 @@ def get_dilated_conv_out_size(in_size, k, s, p, d):
         return o_h
 
 
+# 计算函数用时
+def calc_time_used(func, args=[], count=1):
+    import datetime
+    start_time = datetime.datetime.now()
+    for i in range(count):
+        func(*args)
+    end_time = datetime.datetime.now()
+    return (end_time - start_time).total_seconds()
 
 
