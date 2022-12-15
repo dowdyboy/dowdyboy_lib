41463647d0bf5774fe6b14fac9349645c3f99b44
dowdyboy <1203550038@qq.com>
2022-12-15 13:27:52 +0800

    增加paddle冻结和解冻方法

--- dowdyboy_lib/paddle/model_util.py
+++ dowdyboy_lib/paddle/model_util.py
@@ -20,3 +20,31 @@ def print_model(model, input_size, custom_ops=None, ):
         print(f'input shape: {bat_x.shape}, output shape: {[y.shape for y in pred_y]}')
     else:
         print(f'input shape: {bat_x.shape}, output shape: {pred_y.shape}')
+
+
+def frozen_layer(layer):
+    for v in layer.parameters():
+        v.trainable = False
+
+
+def unfrozen_layer(layer):
+    for v in layer.parameters():
+        v.trainable = True
+
+# def init_model(model):
+#     def reset_func(m):
+#         if isinstance(m, nn.Linear):
+#             trunc_normal_(m.weight, std=.02)
+#             if isinstance(m, nn.Linear) and m.bias is not None:
+#                 constant_(m.bias, 0)
+#         elif isinstance(m, nn.LayerNorm):
+#             constant_(m.bias, 0)
+#             constant_(m.weight, 1.0)
+#         elif hasattr(m, 'weight') and (not isinstance(
+#                 m, (nn.BatchNorm, nn.BatchNorm2D))):
+#             kaiming_uniform_(m.weight, a=math.sqrt(5))
+#             if m.bias is not None:
+#                 fan_in, _ = _calculate_fan_in_and_fan_out(m.weight)
+#                 bound = 1 / math.sqrt(fan_in)
+#                 uniform_(m.bias, -bound, bound)
+#     model.apply(reset_func)
